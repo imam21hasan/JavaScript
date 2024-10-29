5258c9d00bdf64884564a2cb1dad36f4ca9a9936
Imam Hasan Shawon <152065554+imam21hasan@users.noreply.github.com>
2024-10-29 21:27:36 +0600

    Create Array-1.js

---
+++ JS Code/Array-1.js
@@ -0,0 +1,11 @@
+const arr = ["SHWADHEEN", "SHAWON", "NAYON", 17, 28, 29, true, false];
+
+console.log('\nLength of the array is : '+ arr.length);
+
+console.log(arr);
+console.log(arr[1]);
+console.log(arr[5]);
+
+console.log(arr.toString());
+
+console.log(typeof arr);
